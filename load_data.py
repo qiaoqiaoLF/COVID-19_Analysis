77332a8045c3087b8d110618639aafd1c32297c5
fengmingyang666 <106506252+fengmingyang666@users.noreply.github.com>
2022-07-09 15:29:48 +0800

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,1 +1,8 @@
 #TODO load and process the raw data
+import pandas as pd
+def read_country(a):
+    
+    data = pd.read_csv('owid-covid-data.csv')
+    continent = data.groupby('location')
+    con=continent.get_group(a)
+    return con
