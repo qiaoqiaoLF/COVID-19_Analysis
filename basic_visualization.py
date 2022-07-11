2fd4128529e6164c43780554b85c732085cf45c0
qiaoqiaoLF <qwerewqwer4@gmail.com>
2022-07-11 13:08:57 +0800

    add analyze_vaccination_and_hospitalization

--- basic_visualization.py
+++ basic_visualization.py
@@ -1,4 +1,4 @@
-#TODO visualize the data
+# TODO visualize the data
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,46 +9,92 @@ from folium import plugins
 import plotly.express as px
 import scipy
 import load_data
-def plot(x,y):
+
+
+def plot(x, y):
     plt.figure()
-    plt.plot(x,y)
-    for a,b in zip(x,y):
-        plt.text(a,b,(a,b),ha='center',va='bottom',fontsize=15)
+    plt.plot(x, y)
+    for a, b in zip(x, y):
+        plt.text(a, b, (a, b), ha="center", va="bottom", fontsize=15)
     plt.show()
-#x = [1,2,3,4,5]
-#y = [2,5,7,3,4]
-#plot(x,y)
+
+
+# x = [1,2,3,4,5]
+# y = [2,5,7,3,4]
+# plot(x,y)
+
 
 def draw_new_cases():
-    confirmed_global_OWID = load_data.read_data('./data/owid-covid-data.csv')
-    confirmed_global_OWID = load_data.extract_data(confirmed_global_OWID,['location','date','new_cases_smoothed'])
-    fig_global = px.line(confirmed_global_OWID,line_group= 'location' , x= 'date', y='new_cases_smoothed',color_discrete_sequence= px.colors.qualitative.D3,color="location", title="New cases in the world")
-    plotly.offline.plot(fig_global, filename='./result/plotly_global.html')
-    
+    confirmed_global_OWID = load_data.read_data("./data/owid-covid-data.csv")
+    confirmed_global_OWID = load_data.extract_data(
+        confirmed_global_OWID, ["location", "date", "new_cases_smoothed"]
+    )
+    fig_global = px.line(
+        confirmed_global_OWID,
+        line_group="location",
+        x="date",
+        y="new_cases_smoothed",
+        color_discrete_sequence=px.colors.qualitative.D3,
+        color="location",
+        title="New cases in the world",
+    )
+    plotly.offline.plot(fig_global, filename="./result/plotly_global.html")
+
+
 def draw_map_of_total_cases_global(map):
-    confirmed_global_JHU = load_data.read_data('./data/time_series_covid19_confirmed_global.csv')
-    confirmed_global_JHU = load_data.extract_data(confirmed_global_JHU,['Country/Region','Province/State','Long','Lat','7/5/22'])
-    confirmed_global_JHU = confirmed_global_JHU.drop(index=[42,43,53,89,106,175,281])
+    confirmed_global_JHU = load_data.read_data(
+        "./data/time_series_covid19_confirmed_global.csv"
+    )
+    confirmed_global_JHU = load_data.extract_data(
+        confirmed_global_JHU,
+        ["Country/Region", "Province/State", "Long", "Lat", "7/5/22"],
+    )
+    confirmed_global_JHU = confirmed_global_JHU.drop(
+        index=[42, 43, 53, 89, 106, 175, 281]
+    )
     incidents = plugins.MarkerCluster().add_to(map)
-    for country ,province,lat, long, number in zip(confirmed_global_JHU['Country/Region'],confirmed_global_JHU['Province/State'],confirmed_global_JHU['Lat'],confirmed_global_JHU['Long'],confirmed_global_JHU['7/5/22']):
-        folium.Marker([lat, long], popup=(str(country) +'-'+ (str(province)) + '-' +str(number))).add_to(incidents)
+    for country, province, lat, long, number in zip(
+        confirmed_global_JHU["Country/Region"],
+        confirmed_global_JHU["Province/State"],
+        confirmed_global_JHU["Lat"],
+        confirmed_global_JHU["Long"],
+        confirmed_global_JHU["7/5/22"],
+    ):
+        folium.Marker(
+            [lat, long],
+            popup=(str(country) + "-" + (str(province)) + "-" + str(number)),
+        ).add_to(incidents)
     map.add_child(incidents)
-    
-def draw_map_of_total_cases_US(map):    
-    confirmed_US_JHU = load_data.read_data('./data/time_series_covid19_confirmed_US.csv')
-    confirmed_US_JHU = load_data.extract_data(confirmed_US_JHU,['Province_State','Admin2','Lat','Long_','7/5/22'])
+
+
+def draw_map_of_total_cases_US(map):
+    confirmed_US_JHU = load_data.read_data(
+        "./data/time_series_covid19_confirmed_US.csv"
+    )
+    confirmed_US_JHU = load_data.extract_data(
+        confirmed_US_JHU, ["Province_State", "Admin2", "Lat", "Long_", "7/5/22"]
+    )
     incidents = plugins.MarkerCluster().add_to(map)
-    for province,city,lat, long, number in zip(confirmed_US_JHU['Province_State'],confirmed_US_JHU['Admin2'],confirmed_US_JHU['Lat'],confirmed_US_JHU['Long_'],confirmed_US_JHU['7/5/22']):
-        folium.Marker([lat, long], popup=('US' +'-'+ (str(province)) +'-'+ str(city) +  '-' +str(number))).add_to(incidents)
+    for province, city, lat, long, number in zip(
+        confirmed_US_JHU["Province_State"],
+        confirmed_US_JHU["Admin2"],
+        confirmed_US_JHU["Lat"],
+        confirmed_US_JHU["Long_"],
+        confirmed_US_JHU["7/5/22"],
+    ):
+        folium.Marker(
+            [lat, long],
+            popup=("US" + "-" + (str(province)) + "-" + str(city) + "-" + str(number)),
+        ).add_to(incidents)
     map.add_child(incidents)
-    
-  
-    
+
+
 def main():
     draw_new_cases()
-    map =  folium.Map( location=[0,0], zoom_start=2)
+    map = folium.Map(location=[0, 0], zoom_start=2)
     draw_map_of_total_cases_global(map)
     draw_map_of_total_cases_US(map)
-    map.save('./result/map.html')
+    map.save("./result/map.html")
+
 
 main()
--- further_analysis.py
+++ further_analysis.py
@@ -1,1 +1,103 @@
-#TODO analyze the relation
+# TODO analyze the relation
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import folium
+import sklearn
+import plotly
+from folium import plugins
+import plotly.express as px
+import scipy
+import load_data
+
+
+def analyze_vaccination_and_hospitalization():
+    data_OWID = load_data.read_data("./data/owid-covid-data.csv")
+    data_OWID = load_data.extract_data(
+        data_OWID,
+        [
+            "new_cases_per_million",
+            "weekly_icu_admissions_per_million",
+            "weekly_hosp_admissions_per_million",
+            "people_vaccinated_per_hundred",
+            "people_fully_vaccinated_per_hundred",
+        ],
+    )
+    icu = np.array(data_OWID["weekly_icu_admissions_per_million"])
+    hosp = np.array(data_OWID["weekly_hosp_admissions_per_million"])
+    total = np.array(data_OWID["new_cases_per_million"])
+    vac = np.array(data_OWID["people_vaccinated_per_hundred"])
+    f_vac = np.array(data_OWID["people_fully_vaccinated_per_hundred"])
+
+    valid_icu_vac = (
+        (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(vac))
+    )
+    valid_hosp_vac = (
+        (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(vac))
+    )
+    valid_icu_f = (
+        (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(f_vac))
+    )
+    valid_hosp_f = (
+        (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(f_vac))
+    )
+
+    icu_percentage = icu[valid_icu_vac] / total[valid_icu_vac]
+    hosp_percentage = hosp[valid_hosp_vac] / total[valid_hosp_vac]
+    icu_percentage_f = icu[valid_icu_f] / total[valid_icu_f]
+    hosp_percentage_f = hosp[valid_hosp_f] / total[valid_hosp_f]
+
+    from sklearn.svm import SVR
+
+    model = SVR()
+    model.fit(
+        f_vac[valid_hosp_f][hosp_percentage_f < 5].reshape(-1, 1),
+        hosp_percentage_f[hosp_percentage_f < 5],
+    )
+    X_fit = np.arange(100).reshape(-1, 1)
+    Y_fit = model.predict(X_fit)
+    plt.figure()
+    plt.plot(X_fit, Y_fit)
+    plt.savefig("./result/hosp_relation_with_fully_vaccination.png")
+
+    model = SVR()
+    model.fit(
+        vac[valid_hosp_vac][hosp_percentage < 5].reshape(-1, 1),
+        hosp_percentage[hosp_percentage < 5],
+    )
+    X_fit = np.arange(100).reshape(-1, 1)
+    Y_fit = model.predict(X_fit)
+    plt.figure()
+    plt.plot(X_fit, Y_fit)
+    plt.savefig("./result/hosp_relation_with_vaccination.png")
+
+    model = SVR()
+    model.fit(
+        vac[valid_icu_vac][icu_percentage < 5].reshape(-1, 1),
+        icu_percentage[icu_percentage < 5],
+    )
+    X_fit = np.arange(100).reshape(-1, 1)
+    Y_fit = model.predict(X_fit)
+    plt.figure()
+    plt.plot(X_fit, Y_fit)
+    plt.savefig("./result/icu_relation_with_vaccination.png")
+
+    model = SVR()
+    model.fit(
+        f_vac[valid_icu_f][icu_percentage_f < 5].reshape(-1, 1),
+        icu_percentage_f[icu_percentage_f < 5],
+    )
+    X_fit = np.arange(100).reshape(-1, 1)
+    Y_fit = model.predict(X_fit)
+    plt.figure()
+    plt.plot(X_fit, Y_fit)
+    plt.savefig("./result/icu_relation_with_fully_vaccination.png")
+
+
+def main():
+    analyze_vaccination_and_hospitalization()
+
+
+main()
