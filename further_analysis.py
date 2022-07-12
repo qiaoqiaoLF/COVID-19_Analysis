e1cd6ace69bc2595a6cf03738cc039bca303a599
qiaoqiaoLF <qwerewqwer4@gmail.com>
2022-07-12 18:44:11 +0800

    optimize

--- further_analysis.py
+++ further_analysis.py
@@ -11,136 +11,171 @@ from folium import plugins
 import plotly.express as px
 import scipy
 import load_data
+from sklearn.svm import SVR
 
 
 def analyze_vaccination_and_hospitalization():
-    # read data
-    data_OWID = load_data.read_data("./data/owid-covid-data.csv")
-    data_OWID = load_data.extract_data(
-        data_OWID,
-        [
-            "new_cases_per_million",
-            "weekly_icu_admissions_per_million",
-            "weekly_hosp_admissions_per_million",
-            "people_vaccinated_per_hundred",
-            "people_fully_vaccinated_per_hundred",
-            "total_boosters_per_hundred",
-        ],
-    )
-    icu = np.array(data_OWID["weekly_icu_admissions_per_million"])
-    hosp = np.array(data_OWID["weekly_hosp_admissions_per_million"])
-    total = np.array(data_OWID["new_cases_per_million"])
-    vac = np.array(data_OWID["people_vaccinated_per_hundred"])
-    f_vac = np.array(data_OWID["people_fully_vaccinated_per_hundred"])
-    bos = np.array(data_OWID["total_boosters_per_hundred"])
+    def analyzer(target1,target2,knl):
+        """helper function to analyze the relation between vaccination and COVID
 
-    # process data
-    valid_icu_vac = (
-        (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(vac))
-    )
-    valid_hosp_vac = (
-        (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(vac))
-    )
-    valid_icu_f = (
-        (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(f_vac))
-    )
-    valid_hosp_f = (
-        (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(f_vac))
-    )
-    valid_icu_bos = (
-        (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(bos))
-    )
-    valid_hosp_bos = (
-        (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(bos))
-    )
+        Args:
+            target1 (str): covid data
+            target2 (str): vaccination data
+            knl (str): kernel function
+        """        
+        data_OWID = load_data.read_data("./data/owid-covid-data.csv")
+        data_OWID = load_data.extract_data(data_OWID,[target1,target2,"new_cases_per_million"])
+        data_target1 = np.array( data_OWID[target1] )
+        data_target2 = np.array( data_OWID[target2] )
+        data_total = np.array( data_OWID["new_cases_per_million"] )
+        valid_data = (~np.isnan(data_target1)) * (~np.isnan(data_target2)) * (~np.isnan(data_total)) * (data_total > 1)
+        target1_percentage = data_target1[valid_data] / data_total[valid_data]
+        model = sklearn.svm.SVR(kernel=knl)
+        model.fit(data_target2[valid_data].reshape(-1,1)[target1_percentage < 5], target1_percentage[target1_percentage < 5])
+        X_fit = np.arange(100).reshape(-1, 1)
+        Y_fit = model.predict(X_fit)
+        plt.figure()
+        plt.plot(X_fit, Y_fit)
+        plt.xlabel(target1)
+        plt.ylabel(target2)
+        plt.savefig("./result/" + target1  + "_relation_with" + target2 + ".png")        
+        
+    analyzer("weekly_icu_admissions_per_million","people_vaccinated_per_hundred","rbf")
+    analyzer("weekly_icu_admissions_per_million","people_fully_vaccinated_per_hundred","rbf")
+    analyzer("weekly_icu_admissions_per_million","total_boosters_per_hundred","rbf")
+    analyzer("weekly_hosp_admissions_per_million","people_vaccinated_per_hundred","rbf")
+    analyzer("weekly_hosp_admissions_per_million","people_fully_vaccinated_per_hundred","rbf")
+    analyzer("weekly_hosp_admissions_per_million","total_boosters_per_hundred","poly")
+ 
 
-    icu_percentage = icu[valid_icu_vac] / total[valid_icu_vac]
-    hosp_percentage = hosp[valid_hosp_vac] / total[valid_hosp_vac]
-    icu_percentage_f = icu[valid_icu_f] / total[valid_icu_f]
-    hosp_percentage_f = hosp[valid_hosp_f] / total[valid_hosp_f]
-    icu_percentage_bos = icu[valid_icu_bos] / total[valid_icu_bos]
-    hosp_percentage_bos = hosp[valid_hosp_bos] / total[valid_hosp_bos]
 
-    # regression
-    from sklearn.svm import SVR
+        
+    # # read data
+    # data_OWID = load_data.read_data("./data/owid-covid-data.csv")
+    # data_OWID = load_data.extract_data(
+    #     data_OWID,
+    #     [
+    #         "new_cases_per_million",
+    #         "weekly_icu_admissions_per_million",
+    #         "weekly_hosp_admissions_per_million",
+    #         "people_vaccinated_per_hundred",
+    #         "people_fully_vaccinated_per_hundred",
+    #         "total_boosters_per_hundred",
+    #     ],
+    # )
+    # icu = np.array(data_OWID["weekly_icu_admissions_per_million"])
+    # hosp = np.array(data_OWID["weekly_hosp_admissions_per_million"])
+    # total = np.array(data_OWID["new_cases_per_million"])
+    # vac = np.array(data_OWID["people_vaccinated_per_hundred"])
+    # f_vac = np.array(data_OWID["people_fully_vaccinated_per_hundred"])
+    # bos = np.array(data_OWID["total_boosters_per_hundred"])       
+    # # process data
+    # valid_icu_vac = (
+    #     (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(vac))
+    # )
+    # valid_hosp_vac = (
+    #     (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(vac))
+    # )
+    # valid_icu_f = (
+    #     (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(f_vac))
+    # )
+    # valid_hosp_f = (
+    #     (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(f_vac))
+    # )
+    # valid_icu_bos = (
+    #     (~np.isnan(icu)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(bos))
+    # )
+    # valid_hosp_bos = (
+    #     (~np.isnan(hosp)) * (~np.isnan(total)) * (total > 1) * (~np.isnan(bos))
+    # )
 
-    model = SVR()
-    model.fit(
-        f_vac[valid_hosp_f][hosp_percentage_f < 5].reshape(-1, 1),
-        hosp_percentage_f[hosp_percentage_f < 5],
-    )
-    X_fit = np.arange(100).reshape(-1, 1)
-    Y_fit = model.predict(X_fit)
-    plt.figure()
-    plt.plot(X_fit, Y_fit)
-    plt.xlabel("People fully vaccinated per hundred")
-    plt.ylabel("Hospitalization percentage")
-    plt.savefig("./result/hosp_relation_with_fully_vaccination.png")
+    # icu_percentage = icu[valid_icu_vac] / total[valid_icu_vac]
+    # hosp_percentage = hosp[valid_hosp_vac] / total[valid_hosp_vac]
+    # icu_percentage_f = icu[valid_icu_f] / total[valid_icu_f]
+    # hosp_percentage_f = hosp[valid_hosp_f] / total[valid_hosp_f]
+    # icu_percentage_bos = icu[valid_icu_bos] / total[valid_icu_bos]
+    # hosp_percentage_bos = hosp[valid_hosp_bos] / total[valid_hosp_bos]
 
-    model = SVR()
-    model.fit(
-        vac[valid_hosp_vac][hosp_percentage < 5].reshape(-1, 1),
-        hosp_percentage[hosp_percentage < 5],
-    )
-    X_fit = np.arange(100).reshape(-1, 1)
-    Y_fit = model.predict(X_fit)
-    plt.figure()
-    plt.plot(X_fit, Y_fit)
-    plt.xlabel("People vaccinated per 100,000")
-    plt.ylabel("Hospitalization percentage")
-    plt.savefig("./result/hosp_relation_with_vaccination.png")
+    # # regression
 
-    model = SVR()
-    model.fit(
-        vac[valid_icu_vac][icu_percentage < 5].reshape(-1, 1),
-        icu_percentage[icu_percentage < 5],
-    )
-    X_fit = np.arange(100).reshape(-1, 1)
-    Y_fit = model.predict(X_fit)
-    plt.figure()
-    plt.plot(X_fit, Y_fit)
-    plt.xlabel("People vaccinated per 100,000")
-    plt.ylabel("ICU admissions per 100,000")
-    plt.savefig("./result/icu_relation_with_vaccination.png")
 
-    model = SVR()
-    model.fit(
-        f_vac[valid_icu_f][icu_percentage_f < 5].reshape(-1, 1),
-        icu_percentage_f[icu_percentage_f < 5],
-    )
-    X_fit = np.arange(100).reshape(-1, 1)
-    Y_fit = model.predict(X_fit)
-    plt.figure()
-    plt.plot(X_fit, Y_fit)
-    plt.xlabel("Fully vaccinated percentage")
-    plt.ylabel("ICU percentage")
-    plt.savefig("./result/icu_relation_with_fully_vaccination.png")
+    # model = SVR()
+    # model.fit(
+    #     f_vac[valid_hosp_f][hosp_percentage_f < 5].reshape(-1, 1),
+    #     hosp_percentage_f[hosp_percentage_f < 5],
+    # )
+    # X_fit = np.arange(100).reshape(-1, 1)
+    # Y_fit = model.predict(X_fit)
+    # plt.figure()
+    # plt.plot(X_fit, Y_fit)
+    # plt.xlabel("People fully vaccinated per hundred")
+    # plt.ylabel("Hospitalization percentage")
+    # plt.savefig("./result/hosp_relation_with_fully_vaccination.png")
 
-    model = SVR()
-    model.fit(
-        bos[valid_icu_bos][icu_percentage_bos < 5].reshape(-1, 1),
-        icu_percentage_bos[icu_percentage_bos < 5],
-    )
-    X_fit = np.arange(100).reshape(-1, 1)
-    Y_fit = model.predict(X_fit)
-    plt.figure()
-    plt.plot(X_fit, Y_fit)
-    plt.xlabel("People vaccinated per 100,000")
-    plt.ylabel("ICU admissions per 100,000")
-    plt.savefig("./result/icu_relation_with_boosters.png")
+    # model = SVR()
+    # model.fit(
+    #     vac[valid_hosp_vac][hosp_percentage < 5].reshape(-1, 1),
+    #     hosp_percentage[hosp_percentage < 5],
+    # )
+    # X_fit = np.arange(100).reshape(-1, 1)
+    # Y_fit = model.predict(X_fit)
+    # plt.figure()
+    # plt.plot(X_fit, Y_fit)
+    # plt.xlabel("People vaccinated per 100,000")
+    # plt.ylabel("Hospitalization percentage")
+    # plt.savefig("./result/hosp_relation_with_vaccination.png")
 
-    model = SVR(kernel="poly")
-    model.fit(
-        bos[valid_hosp_bos][hosp_percentage_bos < 5].reshape(-1, 1),
-        hosp_percentage_bos[hosp_percentage_bos < 5],
-    )
-    X_fit = np.arange(100).reshape(-1, 1)
-    Y_fit = model.predict(X_fit)
-    plt.figure()
-    plt.plot(X_fit, Y_fit)
-    plt.xlabel("Boosters per 100,000")
-    plt.ylabel("Hospitalization rate per 100,000")
-    plt.savefig("./result/hosp_relation_with_boosters.png")
+    # model = SVR()
+    # model.fit(
+    #     vac[valid_icu_vac][icu_percentage < 5].reshape(-1, 1),
+    #     icu_percentage[icu_percentage < 5],
+    # )
+    # X_fit = np.arange(100).reshape(-1, 1)
+    # Y_fit = model.predict(X_fit)
+    # plt.figure()
+    # plt.plot(X_fit, Y_fit)
+    # plt.xlabel("People vaccinated per 100,000")
+    # plt.ylabel("ICU admissions per 100,000")
+    # plt.savefig("./result/icu_relation_with_vaccination.png")
+
+    # model = SVR()
+    # model.fit(
+    #     f_vac[valid_icu_f][icu_percentage_f < 5].reshape(-1, 1),
+    #     icu_percentage_f[icu_percentage_f < 5],
+    # )
+    # X_fit = np.arange(100).reshape(-1, 1)
+    # Y_fit = model.predict(X_fit)
+    # plt.figure()
+    # plt.plot(X_fit, Y_fit)
+    # plt.xlabel("Fully vaccinated percentage")
+    # plt.ylabel("ICU percentage")
+    # plt.savefig("./result/icu_relation_with_fully_vaccination.png")
+
+    # model = SVR()
+    # model.fit(
+    #     bos[valid_icu_bos][icu_percentage_bos < 5].reshape(-1, 1),
+    #     icu_percentage_bos[icu_percentage_bos < 5],
+    # )
+    # X_fit = np.arange(100).reshape(-1, 1)
+    # Y_fit = model.predict(X_fit)
+    # plt.figure()
+    # plt.plot(X_fit, Y_fit)
+    # plt.xlabel("People vaccinated per 100,000")
+    # plt.ylabel("ICU admissions per 100,000")
+    # plt.savefig("./result/icu_relation_with_boosters.png")
+
+    # model = SVR(kernel="poly")
+    # model.fit(
+    #     bos[valid_hosp_bos][hosp_percentage_bos < 5].reshape(-1, 1),
+    #     hosp_percentage_bos[hosp_percentage_bos < 5],
+    # )
+    # X_fit = np.arange(100).reshape(-1, 1)
+    # Y_fit = model.predict(X_fit)
+    # plt.figure()
+    # plt.plot(X_fit, Y_fit)
+    # plt.xlabel("Boosters per 100,000")
+    # plt.ylabel("Hospitalization rate per 100,000")
+    # plt.savefig("./result/hosp_relation_with_boosters.png")
 
 
 def analyze_smokers_and_COVID():
