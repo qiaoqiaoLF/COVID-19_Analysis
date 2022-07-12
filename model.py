48aca5c56869e9024d21afff8f5a1459349a9c53
qiaoqiaoLF <qwerewqwer4@gmail.com>
2022-07-12 17:25:36 +0800

    add some analysis

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
-#TODO build a NN
-from pickletools import optimize
+# TODO build a NN
 import torch
 import torch.nn as nn
 import numpy as np
@@ -7,8 +6,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import load_data
 
-data_raw = load_data.read_data('./data/owid-covid-data.csv','location','World')
-data = load_data.extract_data(data_raw,['date','new_cases_smoothed'])
-length = data.shape[0]
-Y = data['new_cases_smoothed']
-X = np.arange(length)
+data_raw = load_data.read_data("./data/owid-covid-data.csv")
+data = load_data.extract_data(
+    data_raw,
+    [
+        'reproduction_rate',
+        'total_vaccinations',
+        'people_vaccinated',
+        'people_fully_vaccinated',
+        'total_boosters',
+        'new_vaccinations',
+        'new_vaccinations_smoothed',
+        'total_vaccinations_per_hundred',
+        'people_vaccinated_per_hundred',
+        'people_fully_vaccinated_per_hundred',
+        'total_boosters_per_hundred',
+        'new_vaccinations_smoothed_per_million',
+        'new_people_vaccinated_smoothed',
+        'new_people_vaccinated_smoothed_per_hundred',
+        'stringency_index',
+        'population',
+        'population_density',
+        'median_age',
+        'aged_65_older',
+        'aged_70_older',
+        'gdp_per_capita',
+        'extreme_poverty',
+        'cardiovasc_death_rate',
+        'diabetes_prevalence',
+        'female_smokers',
+        'male_smokers',
+        'handwashing_facilities',
+        'hospital_beds_per_thousand',
+        'life_expectancy',
+        'human_development_index',
+    ],
+)
+print(data)
