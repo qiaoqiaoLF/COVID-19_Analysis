ad67d21091c1f85011293d2a7d331887a3ebc984
qiaoqiaoLF <qwerewqwer4@gmail.com>
2022-07-13 20:55:28 +0800

    Modify LSTM_MODEL

--- LSTM_model.py
+++ LSTM_model.py
@@ -1,27 +1,33 @@
 # TODO build a NN
-from operator import length_hint
 import torch
 import torch.nn as nn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import load_data
+import math
+
 
 def normalize(data):
     return np.log(data + 1)
 
+
 def denormalize(data):
-    return  np.exp(data) - 1
+    return np.exp(data) - 1
 
-data_raw = load_data.read_data("./data/owid-covid-data.csv",'location','World')
 
-sequence_length = 3
-data_cases = np.array(load_data.extract_data(data_raw,['new_cases_smoothed'])).reshape(-1)
+data_raw = load_data.read_data("./data/owid-covid-data.csv", "location", "World")
+
+sequence_length_X = 50
+sequence_length_Y = 30
+data_cases = np.array(load_data.extract_data(data_raw, ["new_cases_smoothed"])).reshape(
+    -1
+)
 data_days = np.arange(data_cases.shape[0]).reshape(-1)
 data_cases = data_cases[100:]
 data_days = data_days[100:]
 
-leftover = data_cases.shape[0] % sequence_length
+leftover = data_cases.shape[0] % math.lcm(sequence_length_X, sequence_length_Y)
 data_cases = data_cases[leftover:]
 data_days = data_days[leftover:]
 valid_data = ~np.isnan(data_cases)
@@ -30,41 +36,58 @@ data_cases = data_cases[valid_data]
 data_days = data_days[valid_data]
 
 
-length = data_cases.shape[0] - sequence_length
+length = data_cases.shape[0] - sequence_length_X - sequence_length_Y
 
-data_days_X = data_days[:length]
+data_days_X_train = data_days[:length]
+data_days_X_test = data_days[length:length + sequence_length_Y]
 
-data_cases_X = np.zeros((length,sequence_length))
-data_cases_Y = np.zeros((length,1))
+data_cases_X = np.zeros((length, sequence_length_X))
+data_cases_Y = np.zeros((length, sequence_length_Y))
 for i in range(length):
-    data_cases_X[i,:] = data_cases[i:i+sequence_length]
-    data_cases_Y[i,:] = data_cases[i+sequence_length]
+    data_cases_X[i, :] = data_cases[i : i + sequence_length_X]
+    data_cases_Y[i, :] = data_cases[
+        i + sequence_length_X : i + sequence_length_X + sequence_length_Y
+    ]
 
 
+train_X = torch.tensor(
+    data_cases_X.reshape(-1, sequence_length_X, 1), dtype=torch.float
+)
+train_Y = torch.tensor(data_cases_Y.reshape(-1, sequence_length_Y), dtype=torch.float)
 
-train_X = torch.tensor(data_cases_X.reshape(-1,sequence_length,1), dtype=torch.float)
-train_Y = torch.tensor(data_cases_Y.reshape(-1,1,1), dtype=torch.float)
-
+test_X = torch.tensor(
+    data_cases[-sequence_length_X - sequence_length_Y : -sequence_length_Y].reshape(
+        -1, sequence_length_X, 1
+    ),
+    dtype=torch.float,
+)
+test_Y = torch.tensor(
+    data_cases[-sequence_length_Y:].reshape(-1, sequence_length_Y), dtype=torch.float
+)
 
 
 class LSTM(nn.Module):
     def __init__(self):
         super(LSTM, self).__init__()
-        self.lstm = nn.LSTM(input_size=1, hidden_size=100, num_layers=1, batch_first=True)
-        self.linear = nn.Linear(100 * sequence_length, 1)
+        self.lstm = nn.LSTM(
+            input_size=1, hidden_size=100, num_layers=1, batch_first=True
+        )
+        self.linear = nn.Linear(100 * sequence_length_X, sequence_length_Y)
+
     def forward(self, x):
         x, (h, c) = self.lstm(x)
-        x = x.reshape(-1, 100 * sequence_length)
+        x = x.reshape(-1, 100 * sequence_length_X)
         x = self.linear(x)
         return x
 
+
 # 模型训练
 model = LSTM()
-optimzer = torch.optim.Adam(model.parameters(), lr=0.005)
+optimzer = torch.optim.Adam(model.parameters(), lr=0.01)
 loss_func = nn.MSELoss()
 model.train()
 l = []
-epochs = 1000
+epochs = 10000
 for i in range(epochs):
     output = model(train_X)
     loss = loss_func(output, train_Y)
@@ -72,13 +95,39 @@ for i in range(epochs):
     optimzer.zero_grad()
     loss.backward()
     optimzer.step()
-    if i % 100 == 0:       
-        print("i:{}, train_loss:{}".format(i, loss))
+    if i % 100 == 99:
+        print("i:{}, train_loss:{}".format(i + 1, loss))
 
 
-y_predict = model(train_X)
+y_train_predict = model(train_X)
+y_test_predict = model(test_X)
 
 plt.figure()
-plt.plot(data_days_X.reshape(-1) ,denormalize(y_predict.detach().numpy().reshape(-1)),label='predict')
+plt.plot(
+    data_days_X_train.reshape(-1),
+    denormalize(y_train_predict.detach().numpy()[:, 0].reshape(-1)),
+    label="predict train",
+    c="b",
+)
+plt.plot(
+    data_days_X_train.reshape(-1),
+    denormalize(train_Y.detach().numpy()[:, 0].reshape(-1)),
+    label="true train",
+    c="r",
+)
+
+plt.plot(
+    data_days_X_test.reshape(-1),
+    denormalize(y_test_predict.detach().numpy()[:, :].reshape(-1)),
+    label="predict test",
+    c="g",
+)
+plt.plot(
+    data_days_X_test.reshape(-1),
+    denormalize(test_Y.detach().numpy()[:, :].reshape(-1)),
+    label="true test",
+    c="y",
+)
+plt.legend()
 plt.show()
 
